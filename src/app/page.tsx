d4defb95acddf73bde8d75f72d5cd3df0ab9272d
samarth nagar <samarth07nagar@gmail.com>
2026-03-28 01:39:06 +0530

    added database via neon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
 import { mockImages } from "./lib/data";
+import { db } from "../server/db";
 
-export default function HomePage() {
+
+
+export default async function HomePage() {
+    const posts = await db.query.posts.findMany();
+    console.log(posts);
+
 	return (
 		<main className="">
 			hello gallery in work
 			<div className="flex flex-wrap">
+                {posts.map((post)=>(
+                    <div key={post.id}>{post.name}</div>
+                ))}
 				{mockImages.map((image) => (
 					<div key={image.id} className="w-48">
 						<img src={image.url} />
--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,18 +1,8 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
-
-import { env } from "~/env";
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
 import * as schema from "./schema";
 
-/**
- * Cache the database connection in development. This avoids creating a new connection on every HMR
- * update.
- */
-const globalForDb = globalThis as unknown as {
-  conn: postgres.Sql | undefined;
-};
 
-const conn = globalForDb.conn ?? postgres(env.DATABASE_URL);
-if (env.NODE_ENV !== "production") globalForDb.conn = conn;
+const sqlServer = neon(process.env.DATABASE_URL!);
 
-export const db = drizzle(conn, { schema });
+export const db = drizzle(sqlServer, {schema});
