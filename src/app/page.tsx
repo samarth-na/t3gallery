2263baa92ef46dfe83dd93f94b7b36769a1277bc
samarth nagar <samarth07nagar@gmail.com>
2026-03-29 01:13:26 +0530

    backup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,28 @@
-import { mockImages } from "./lib/data";
+import { desc } from "drizzle-orm";
 import { db } from "../server/db";
 
-
+export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
-    const posts = await db.query.posts.findMany();
+    const posts = await db.query.images.findMany({
+        orderBy: (model, { desc }) => desc(model.id),
+    });
     console.log(posts);
 
-	return (
-		<main className="">
-			hello gallery in work
-			<div className="flex flex-wrap">
-                {posts.map((post)=>(
-                    <div key={post.id}>{post.name}</div>
+    return (
+        <main className="">
+            hello gallery in work
+            <div className="flex flex-wrap">
+                {[...posts, ...posts].map((image) => (
+                    <div
+                        key={image.id}
+                        className="w-48 flex flex-col m-2 bg-green-50"
+                    >
+                        <img src={image.url} />
+                        <div>{image.name}</div>
+                    </div>
                 ))}
-				{mockImages.map((image) => (
-					<div key={image.id} className="w-48">
-						<img src={image.url} />
-					</div>
-				))}
-			</div>
-		</main>
-	);
+            </div>
+        </main>
+    );
 }
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -11,16 +11,19 @@ import { index, pgTableCreator } from "drizzle-orm/pg-core";
  */
 export const createTable = pgTableCreator((name) => `t3gallery_${name}`);
 
-export const posts = createTable(
-  "post",
-  (d) => ({
-    id: d.integer().primaryKey().generatedByDefaultAsIdentity(),
-    name: d.varchar({ length: 256 }),
-    createdAt: d
-      .timestamp({ withTimezone: true })
-      .$defaultFn(() => /* @__PURE__ */ new Date())
-      .notNull(),
-    updatedAt: d.timestamp({ withTimezone: true }).$onUpdate(() => new Date()),
-  }),
-  (t) => [index("name_idx").on(t.name)],
+export const images = createTable(
+    "image",
+    (d) => ({
+        id: d.integer().primaryKey().generatedByDefaultAsIdentity(),
+        name: d.varchar({ length: 256 }).notNull(),
+        url: d.varchar({ length: 1024 }).notNull(),
+        createdAt: d
+            .timestamp({ withTimezone: true })
+            .$defaultFn(() => /* @__PURE__ */ new Date())
+            .notNull(),
+        updatedAt: d
+            .timestamp({ withTimezone: true })
+            .$onUpdate(() => new Date()),
+    }),
+    (t) => [index("name_idx").on(t.name)],
 );
